a633f521465b6b6de4e57b75448eec1fcbe9322b
Guilherme Baia <guilhermebaiav@gmail.com>
2023-09-24 23:07:02 -0300

    fixing container's width and text maxwidth

--- quiz-challenge/src/App.jsx
+++ quiz-challenge/src/App.jsx
@@ -13,13 +13,13 @@ function App() {
 
   let questionsArray= [
     {
-      "question": "Qual é a cor do céu?",
-      "a1": "Azul",
-      "a2": "Verde",
-      "a3": "Rosa",
-      "v1": true,
+      "question": "Qual estrutura de dados é mais adequada para representar uma conjunto de elementos com comportamento 'primeiro a entrar, primeiro a sair' (FIFO)?",
+      "a1": "Pilha",
+      "a2": "Lista Ligada",
+      "a3": "Fila",
+      "v1": false,
       "v2": false,
-      "v3": false,
+      "v3": true,
     },
     {
       "question": "Quanto é 3 + 5?",
@@ -50,7 +50,7 @@ function App() {
 
   return (
     <div className='bg-grey-5 w-screen h-screen flex justify-center items-center text-grey-1'>
-    <div className='bg-grey-6 px-36 py-16 rounded-md'>
+    <div className='bg-grey-6 px-36 py-16 rounded-md w-3/5'>
     <div className=''>
       <Header correctAnswers={points}/>
       <QuestionContainer question={questionsArray[questionNumber].question}>
--- quiz-challenge/src/components/QuestionContainer.jsx
+++ quiz-challenge/src/components/QuestionContainer.jsx
@@ -3,7 +3,7 @@ import { Children } from "react";
 export function QuestionContainer({question, children}){
     return (
         <div className="flex flex-col items-center">
-            <p className="my-16 text-2xl font-bold ">{question}</p>
+            <p className="my-16 text-2xl font-bold max-w-3xl text-center">{question}</p>
             <div className="flex justify-center gap-10 mb-16 ">
                 {children}
             </div>
